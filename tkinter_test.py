fb21efc5211c70991ae322557f5a92f03828d344
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2018-09-17 15:36:35 +0200

    Initial commit

---
+++ tkinter_test.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+
+def hello():
+    print("hello")
+
+root = tk.Tk()
+root.geometry('{}x{}'.format(320, 240))
+menubar = tk.Menu(root)
+
+# create a pulldown menu, and add it to the menu bar
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="Open", command=hello)
+filemenu.add_command(label="Save", command=hello)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="Current", menu=filemenu)
+
+# create more pulldown menus
+editmenu = tk.Menu(menubar, tearoff=0)
+editmenu.add_command(label="Cut", command=hello)
+editmenu.add_command(label="Copy", command=hello)
+editmenu.add_command(label="Paste", command=hello)
+menubar.add_cascade(label="Edit", menu=editmenu)
+
+# create more pulldown menus
+helpmenu = tk.Menu(menubar, tearoff=0)
+helpmenu.add_command(label="About", command=hello)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+# display the menu
+root.config(menu=menubar)
+
+root.mainloop()
